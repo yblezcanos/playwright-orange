47ebd18eb4bb043ce289f34a2c40190dc1e8cebe
yblezcanos <yblezcanos@gmail.com>
2025-02-19 23:15:01 +0100

    up changes

--- tests/modules/Profile/login.spec.ts
+++ tests/modules/Profile/login.spec.ts
@@ -62,7 +62,6 @@ test.describe('Login Tests', () => {
     });
 
     test('should not login with a weak password and username less than 5 characters', async ({ page }) => {
-    // Genera un nombre de usuario de menos de 5 caracteres
         const shortUsername = faker.generateUsernameWithXCharacters(4); // Crea un nombre de usuario de 4 caracteres el sistema especifica que deben ser 5
         const weakPass = faker.generatePasswordWithRequirements(3); // Genera una contraseña demasiado corta
         await test.step('Enter weak password and short username', async ()=>{
@@ -74,5 +73,60 @@ test.describe('Login Tests', () => {
             expect(isAlertVisible).toBeTruthy();
             expect(alertText).toBe('Invalid credentials');
         }); 
+    });
+
+    test('should not login with valid username and invalid password', async ({ page }) => {
+        const username = process.env.ADMIN_USERNAME || 'fallbackUser';
+        const invalidPassword = faker.generatePasswordWithRequirements(8)//contraseña inválida pero con formato correcto(más de 7 caracteres y al menos una letra y un número);
+        await test.step('Enter valid username and invalid password', async () => {
+            await loginPage.login(username, invalidPassword);
+        });
+        await test.step('Verify error message', async () => {
+            let isAlertVisible = await loginPage.errorAlertIsShowed();
+            let alertText = await loginPage.errorAlert();
+            expect(isAlertVisible).toBeTruthy();
+            expect(alertText).toBe('Invalid credentials');
+        });
+    });
+
+    test('should not login with invalid username and valid password', async ({ page }) => {
+        const invalidUsername = faker.generateRandomUsername();
+        const password = process.env.ADMIN_PASSWORD || 'fallbackPass';
+        await test.step('Enter invalid username and valid password', async () => {
+            await loginPage.login(invalidUsername, password);
+        });
+        await test.step('Verify error message', async () => {
+            let isAlertVisible = await loginPage.errorAlertIsShowed();
+            let alertText = await loginPage.errorAlert();
+            expect(isAlertVisible).toBeTruthy();
+            expect(alertText).toBe('Invalid credentials');
+        });
+    });
+
+    test('should not login with valid username with leading spaces and valid password', async ({ page }) => {
+        const username = `   ${process.env.ADMIN_USERNAME || 'fallbackUser'}`;
+        const password = process.env.ADMIN_PASSWORD || 'fallbackPass'; // contraseña válida
+        await test.step('Enter valid username with leading spaces and valid password', async () => {
+            await loginPage.login(username, password);
+        });
+        await test.step('Verify error message', async () => {
+            let isAlertVisible = await loginPage.errorAlertIsShowed();
+            let alertText = await loginPage.errorAlert();
+            expect(isAlertVisible).toBeTruthy();
+            expect(alertText).toBe('Invalid credentials');
+        });
+    });
+
+    test('should login with valid username with trailing spaces and valid password', async ({ page }) => {
+        const username = `${process.env.ADMIN_USERNAME || 'fallbackUser'}   `;
+        const password = process.env.ADMIN_PASSWORD || 'fallbackPass'; // contraseña válida
+        await test.step('Enter valid username with trailing spaces and valid password', async () => {
+            await loginPage.login(username, password);
+        });
+        await test.step('Verify login success', async () => {
+            await expect(page).toHaveURL('/web/index.php/dashboard/index');
+            let isTextVisible = await loginPage.isNavbarTextVisible('Dashboard');
+            expect(isTextVisible).toBeTruthy();
+        });
     });
 });
