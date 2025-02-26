a75e7ea2bb86acd92fa6f738fd32f9920a7ab94a
yblezcanos <yblezcanos@gmail.com>
2025-02-26 11:03:42 +0100

    logout up

--- tests/modules/Profile/logout.spec.ts
+++ tests/modules/Profile/logout.spec.ts
@@ -12,23 +12,10 @@ test.describe('Logout Tests', () => {
         basePage = new BasePage(page);
         await basePage.goToDashboard(page);
         await basePage.logout();
-        await expect(page).toHaveURL('/web/index.php/auth/login');        
-    });
-
-    test('should redirect to login if accessing profile after logout', async ({ page }) => {
-        basePage = new BasePage(page);
-        loginPage = new LoginPage(page);
-        const username = process.env.ADMIN_USERNAME || 'fallbackUser';
-        const password = process.env.ADMIN_PASSWORD || 'fallbackPass';
-        await basePage.goto('/web/index.php/auth/login'); // Asegura que inicia desde la página de login
-        await loginPage.login(username, password); // Realiza login explícitamente
-        await basePage.logout(); // Asegura que la sesión se cierra
+        await expect(page).toHaveURL('/web/index.php/auth/login');   
+        let isLogoutButtonVisible = await basePage.isLogoutButtonVisible(page);
+        await expect(isLogoutButtonVisible).toBeFalsy();
         await basePage.goto('/web/index.php/dashboard/index');
-        await expect(page).toHaveURL('/web/index.php/auth/login');
-    });
-
-    test('should not show logout button when not logged in', async ({ page }) => {
-        await page.goto('https://example.com/login');
-        await expect(page.locator('button#logout')).not.toBeVisible();
-    });
+        await expect(page).toHaveURL('/web/index.php/auth/login');     
+    });    
 });
