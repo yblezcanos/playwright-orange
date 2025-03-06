f9cf3419267484d63ea6653db6bf2d14741a34b5
yblezcanos <yblezcanos@gmail.com>
2025-03-06 23:32:23 +0100

    add jsdoc to setup

--- tests/common/auth.setup.ts
+++ tests/common/auth.setup.ts
@@ -1,17 +1,22 @@
+/**
+ * @file auth.setup.ts
+ * @description This file contains the setup script for saving the login state using Playwright.
+ * It imports necessary modules and defines a setup function to save the login session state.
+ */
+
 import { test as setup, expect } from "@playwright/test";
 import { LoginPage } from '../../POM/pages/LoginPage';
 
 const authFile = "tmp/.auth/user.json";
-//const logoutFile = "playwright/.auth/user_logout.json";
 let loginPage: LoginPage;
 
+/**
+ * Setup function to save the login state.
+ * 
+ * @param {Object} param0 - The object containing the page instance.
+ * @param {import('@playwright/test').Page} param0.page - The Playwright page instance.
+ */
 setup("save login state", async ({ page }) => {
-    loginPage = new LoginPage(page);  // Instanciamos la página
+    loginPage = new LoginPage(page);  // Instantiate the LoginPage
     await loginPage.saveSession({page, authFile});
 });
-
-/**
-    const authFile = "playwright/.auth/tmp.logout.json";
-    await loginPage.saveSession({page, authFile});
-    test.use({ storageState: authFile });
- */
